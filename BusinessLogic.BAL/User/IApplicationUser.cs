9f8a716ea273fd05971161b860b0e2023964fb57
Filip Lazarevic <lazarevicfilip22@gmail.com>
2022-12-06 19:11:10 +0100

    Initial commit

---
+++ BusinessLogic.BAL/User/IApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.BAL.User
+{
+    internal interface IApplicationUser
+    {
+        public string Identity { get; }
+        public int Id { get; }
+        public string Email { get; }
+    }
+}
