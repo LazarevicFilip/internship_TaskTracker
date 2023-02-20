fd5614b6bc9594fdacae76165db79bef404b93c0
lazarevicfilip22@gmail.com <lazarevicfilip22@gmail.com>
2023-02-20 14:25:31 +0100

    2

--- Domain/Dto/Auth/UserLoginDto.cs
+++ Domain/Dto/Auth/UserLoginDto.cs
@@ -6,12 +6,9 @@ using System.Threading.Tasks;
 
 namespace Domain.Dto.Auth
 {
-    public class UserRegisterDto
+    public class UserLoginDto
     {
-        public string UserName { get; set; } = string.Empty; 
         public string Email { get; set; } = string.Empty;
-        public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
     }
 }
